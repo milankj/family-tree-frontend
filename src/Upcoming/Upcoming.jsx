6c1e502ca18259f54b27596bd13efdc618b9a66b
milankj <milanjain005@gmail.com>
2022-08-06 16:33:38 +0530

    upcoming colored

--- src/Upcoming/Upcoming.jsx
+++ src/Upcoming/Upcoming.jsx
@@ -5,25 +5,30 @@ export default function Upcoming(props) {
     const d = new Date()
     const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let month = d.getMonth()
-    console.log(monthList[month])
-
+    let currentdate = d.getDate()
     const recentBirth = () => {
         let inputMonth = props.dateofBirth.replace(/[^a-zA-Z]/g, "")
+        let inputDate = parseInt(props.dateofBirth.slice(-2))
+        console.log(props.dateofBirth.slice(-2))
+        const textColor = {color : currentdate < inputDate ? "DodgerBlue" : "#918E9B"}
         if (inputMonth != 'N/A') {
             if (inputMonth === monthList[month]) {
                 return (
-                    <div className='single-news'>{props.fullName} has birthday on {props.dateofBirth.slice(-2)}</div>
+                    <div className='single-news' style={textColor}>{props.fullName} birthday on {props.dateofBirth.slice(-2)}</div>
                 )
             }
             return null
         }
     }
     const recentDeath = () => {
+        let inputDate = parseInt(props.dateofDeath.slice(-2))
+        console.log(props.dateofDeath.slice(-2))
+        const textColor = {color : currentdate < inputDate ? "DodgerBlue" : "#918E9B"}
         let inputMonth = props.dateofDeath.replace(/[^a-zA-Z]/g, "")
         if (inputMonth != 'N/A') {
             if (inputMonth === monthList[month]) {
                 return (
-                    <div className='single-news'>{props.fullName} death aniversary on {props.dateofDeath.slice(-2)}</div>
+                    <div className='single-news' style={textColor}>{props.fullName} death aniversary on {props.dateofDeath.slice(-2)}</div>
                 )
             }
             return null
@@ -31,11 +36,14 @@ export default function Upcoming(props) {
         return null
     }
     const recentMarriage = () => {
+        let inputDate = parseInt(props.dateofMarriage.slice(-2))
+        console.log(props.dateofMarriage.slice(-2))
+        const textColor = {color : currentdate < inputDate ? "DodgerBlue" : "#918E9B"}
         let inputMonth = props.dateofMarriage.replace(/[^a-zA-Z]/g, "")
         if (inputMonth != 'N/A') {
             if (inputMonth === monthList[month]) {
                 return (
-                    <div className='single-news'>{props.fullName} Marriage aniversary on {props.dateofMarriage.slice(-2)}</div>
+                    <div className='single-news' style={textColor}>{props.fullName} Marriage aniversary on {props.dateofMarriage.slice(-2)}</div>
                 )
             }
             return null
