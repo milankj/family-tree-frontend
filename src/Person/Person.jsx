c59011154e6258f5946d0bc7fa4c630820c57453
milankj <milanjain005@gmail.com>
2022-08-06 10:01:48 +0530

    personal css

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation,Outlet } from 'react-router-dom'
+import './Person.css'
 export default function Person(props) {
     const [user, setUser] = useState([])
     useEffect(() => {
@@ -23,7 +24,7 @@ export default function Person(props) {
         user.map(obj=>{
             return(
                 <div key={obj._id} className='personal-details'>
-                    <h3>Name: {obj.fullName}</h3>
+                    <h3>{obj.fullName}</h3>
                     <p>Date of Birth: {obj.dateofBirth}</p>
                     <p>Family Name: {obj.familyName}</p>
                     {obj.dateofMarriage==='N/A'? null : <p>Date of Marriage: {obj.dateofMarriage}</p>}
