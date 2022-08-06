676a16b76cbe864ca2bcf340078c469b166fce60
milankj <milanjain005@gmail.com>
2022-08-06 11:20:48 +0530

    none

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -27,9 +27,10 @@ export default function Person(props) {
                     <h3>{obj.fullName}</h3>
                     <p>Date of Birth: {obj.dateofBirth}</p>
                     <p>Family Name: {obj.familyName}</p>
-                    {obj.dateofMarriage==='N/A'? null : <p>Date of Marriage: {obj.dateofMarriage}</p>}
-                    {obj.dateofDeath==='N/A'? null : <p>Date of Death: {obj.dateofDeath}</p>}
-                    {obj.spouse==='N/A'? null : <p>Spouse: {obj.spouse}</p>}
+                    <p>Place of Residence: {obj.residencePlace}</p>
+                    {obj.dateofMarriage==='N/A' || obj.dateofMarriage==='n/a'? null : <p>Date of Marriage: {obj.dateofMarriage}</p>}
+                    {obj.dateofDeath==='N/A' || obj.dateofDeath==='n/a'? null : <p>Date of Death: {obj.dateofDeath}</p>}
+                    {obj.spouse==='N/A' || obj.spouse==='n/a'? null : <p>Spouse: {obj.spouse}</p>}
                     <Outlet/>
                 </div>
             )
